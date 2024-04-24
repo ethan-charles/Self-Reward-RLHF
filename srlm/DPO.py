ca9df832c96b8d8b5f01980a584426ec61949612
ethan-charles <3219682071@qq.com>
2024-04-24 04:26:55 +0000

    modify the DPO.py

--- srlm/DPO.py
+++ srlm/DPO.py
@@ -5,10 +5,10 @@ from utils.trainer import DpoTraining
 
 def dpo():
     parser = argparse.ArgumentParser(description='DPO finetune a model.')
-    parser.add_argument('-d', '--dataset', default="G:\\我的云端硬盘\\598\\code\\Self-Reward-RLHF\\generated\\preferences.jsonl", type=str, help='input sft dataset')
-    parser.add_argument('-b', '--base_model', default="microsoft/phi-2", type=str, help='the base model we want to fine-tune')
-    parser.add_argument('-m', '--model', default="microsoft/phi-2", type=str, help='the base model we want to fine-tune')
-    parser.add_argument('-o', '--output', default="G:\\我的云端硬盘\\598\\code\\Self-Reward-RLHF\\generated\\", type=str, help='output trained model')
+    parser.add_argument('-d', '--dataset', default="/root/Self-Reward-RLHF/generated/preferences.jsonl", type=str, help='input sft dataset')
+    parser.add_argument('-b', '--base_model', default="Dudep/phi2-sft", type=str, help='the base model we want to fine-tune')
+    parser.add_argument('-m', '--model', default="Dudep/phi2-sft", type=str, help='the base model we want to fine-tune')
+    parser.add_argument('-o', '--output', default="/root/Self-Reward-RLHF/generated-model", type=str, help='output trained model')
     args = parser.parse_args()
 
 
@@ -29,7 +29,7 @@ def dpo():
         lr=lr,
         batch_size=batch_size,
     )
-    trainer.train()
+    trainer()
 
 if __name__ == "__main__":
     dpo()
